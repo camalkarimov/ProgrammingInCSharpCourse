1b89bd2bbf27ae2a13aed6a9618d24ecd1315cf8
Camal <camal.2001kerimov@gmail.com>
2021-10-14 10:16:51 +0400

    First Project

---
+++ Lesson1/FirstLessonProgramming/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FirstLessonProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First Number Please");
+            int var1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Second Number Please");
+
+            int var2 = int.Parse(Console.ReadLine());
+
+            int sum = Sum(var1, var2);
+            Console.WriteLine( "Sum is:"+sum );
+            
+        }
+        public static int Sum(int var1, int var2)
+        {
+            return var1 + var2;
+        }
+    }
+}
---
+++ Lesson2/SecondLessonProgramming/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace SecondLessonProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Task1
+            char letter, letter1, letter2;
+
+            Console.Write("Input letter: ");
+            letter = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Input letter: ");
+            letter1 = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Input letter: ");
+            letter2 = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("{0} {1} {2}", letter2, letter1, letter);
+
+            Console.WriteLine("==============================");
+
+            //Task2
+            Console.WriteLine("Name");
+            var name = Console.ReadLine();
+            Console.WriteLine("Surname");
+            var surname = Console.ReadLine ();
+            string str = name.ToUpper() + " " + surname.ToUpper(); 
+            Console.WriteLine(str);
+
+            Console.WriteLine("==============================");
+            //Task3
+            double r, perimeter, area;
+            Console.WriteLine("Enter radius of circle");
+            r = Convert.ToDouble(Console.ReadLine());
+            perimeter = 2 * 3.14 * r;
+            area = 3.14 * Math.Pow(r, 2);
+            Console.WriteLine();
+            Console.WriteLine("Perimeter of circle" + perimeter);
+            Console.WriteLine();
+            Console.WriteLine("Area of circle" + area);
+
+
+        }
+    }
+}
+
